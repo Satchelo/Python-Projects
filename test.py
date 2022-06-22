4aeba6fdbec3ecf1f1e4f35f1a7c141949657cd0
Satchelo <onur.fb.yasar@outlook.com>
2022-06-23 01:16:26 +0300

    Hello GitHub

---
+++ test.py
@@ -0,0 +1,72 @@
+"""
+Project Owner: Onur YAŞAR
+Start Date: 22/06/2022 17:40
+Last Modified Date: 22/06/2022 00:30
+"""
+
+# print("MERHABA \nONUR YAŞAR")
+# print("merhaba benim adım \tONUR")  
+# print("merhaba benim adım {ad}, yaşım {yas}  ".format(ad="ONUR REİS", yas= 23))
+# onur_yasar=5
+# print(onur_yasar)
+# print(3+9) 
+# print(8%3)
+# print(type(15))
+# strvar="python"     
+# print(strvar)
+# print(strvar[2:5:2])
+# a="onuryasar1907"
+# print(a)
+# print(a[0:5])
+# print(len(a))
+# print(len(strvar))
+# print(strvar+" öğren!")
+# strvar= strvar+ " oğren!"
+# print(strvar.upper())
+# print(strvar.lower())
+# print(strvar.split("o"))
+# print(strvar.split("o", 1))
+# print("onur".center(5))
+# print(strvar.upper())
+# print(strvar.isupper())
+# print(strvar.join([" 123", "onur", "ali"]))
+# print(a.isdigit())
+# recep= "ALİ"
+# print(recep.isdigit())
+# print(recep.isupper())
+
+onurreis = [4,2,3,1,7,6,5]
+onurreis.append("onur") # Add data to list
+print(onurreis)
+onurreis.append("onur") # Add data to list
+print(onurreis)
+
+
+onurreis.insert(3, "reco") # Add data to specific index
+print(onurreis)
+
+onurreis.remove("onur") # Remove data from list
+print(onurreis)
+onurreis.append(10)
+print(onurreis)
+onurreis.pop() # Remove last element
+print(onurreis)
+
+onurreis.remove("onur") # Remove data from list
+print(onurreis)
+onurreis.remove("reco") # Remove data from list
+print(onurreis)
+onurreis.sort() #Sort the list
+print(onurreis)
+
+onurreis.sort(reverse=True) #Sort the list
+print(onurreis)
+
+gdgfgdfdg = "Onur"
+print(gdgfgdfdg)
+
+
+b= "Onur was a good man but he has a problem. he was trusting to everyone. If I could go back in time. I could say: Onur fuck your life"
+print(b.upper())
+b= b.upper()
+print(b.isupper())
