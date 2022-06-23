108b630f2c55577b6e231fadf25f62ddc00973fc
Satchelo <onur.fb.yasar@outlook.com>
2022-06-23 17:56:14 +0300

    list-set, dictionary, tuple are added

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ Last Modified Date: 22/06/2022 00:30
 # print(gdgfgdfdg)
 
 
-# b= "Onur was a good man but he has a problem. he was trusting to everyone. If I could go back in time. I could say: Onur fuck your life"
+# b= "Onur was a good man but he had a problem. he was trusting to everyone. If I could go back in time. I could say: Onur fuck your life"
 # print(b.upper())
 # b= b.upper()
 # print(b.isupper())
@@ -74,10 +74,75 @@ Last Modified Date: 22/06/2022 00:30
 # print("ONUR IS A JUNIOR PYTHON DEVELOPER")
 
 """23/06/2022"""
-a= True
-print(type(a))
-yas1 = 18
-yas2 = 20
-print(yas1 == 18)
-print(yas1 != 18)
-print(not yas2 > 18)
+# a = True
+# print(type(a))
+# yas1 = 18
+# yas2 = 20
+# print(yas1 == 18)
+# print(yas1 != 18)
+# print(not yas2 > 18)
+
+# list1 = ['a','b', 'c', 'd', 'e']
+# list1.append('onur')
+# print(list1)
+# print(list1 + ['yasar'])
+# print(list1.append('fb'))
+# print(list1 + ['3'])
+# list1.pop(0)
+# print(list1)
+# list1 [0] = "123"
+# print(list1)    
+# list1index = list1.index('e')
+# print(list1index)    
+
+# tup = ('a','b', 'c', 'd', 'e', 'a')  #valu of tube is a constant digit
+# tupcount = tup.count('a')  
+# print(tupcount)
+
+# dict1 = {
+#     'name' : 'onur',
+#     'age' : 23,
+#     'city' : 'istanbul'
+# }
+# print(dict1)
+
+# dict2 = {
+#     'name' : 'onur',
+#     'age' : 23,
+#     'city' : {
+#         'born_city' : 'istanbul',
+#         'live_city' : 'aydın'
+#     }
+# }
+# print(dict2)
+# print(dict2['city'])
+# print(dict2['city']['live_city'])
+# ct = dict2.get('city').get('live_city')
+# print(ct)
+# ky = dict1.keys()
+# print(ky)
+# vl = dict1.values()
+# print(vl)
+# it = dict1.items()
+# print(it)
+""" QUIZ 1 """
+
+# print(5*4)
+# print(5%4)
+# print(type(2.3 + 4.7))
+
+# st = 'yakın kampüs'
+# print(st[10])
+# print(st.index('ü'))
+# print(st.upper().lower())
+# print(st.split('a', 1))
+
+# a = True
+# b = False
+# c ='true'
+# print(a == b)
+
+# lt = [1, 'a', 2, 3, True, 4, 5, 'true', '1']
+# print(lt[-1])
+# lt.reverse()
+# print(lt)
